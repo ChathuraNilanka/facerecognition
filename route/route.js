6abd786686859a9f333d6a93b4746abc3e22d470
Chathura Nilanka <chathuranilanka1024@gmail.com>
2020-09-22 23:56:39 +0530

    init

--- route/route.js
+++ route/route.js
@@ -4,7 +4,23 @@ var bodyParser = require('body-parser')
 var db = fire.firestore()
 router.use(bodyParser.json())
 
-router.get('/data', (req, res)=>{
+router.get('/datastudent', (req, res)=>{
+    var type = req.query.type;
+    var allData = [];
+    db.collection('students')
+    .get()
+    .then(snapshot => {
+        snapshot.forEach((hasil)=>{
+            allData.push(hasil.data())
+        })
+        console.log(allData)
+        res.send(allData)
+    }).catch((error)=>{
+        console.log(error)
+    })
+})
+
+router.get('/datadetection', (req, res)=>{
   
     var allData = []
     db.collection('students')
@@ -32,6 +48,16 @@ router.get('/student', (req, res)=>{
     })
 })
 
-
+router.get('/detection', (req, res)=>{
+   
+    db.collection('detection').add({
+        id: req.query.id,
+        name: req.query.name
+    })
+    res.send({
+        id: req.query.id,
+        name: req.query.name
+    })
+})
 
 module.exports = router
